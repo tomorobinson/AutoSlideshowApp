c3f01e4d3516ba5642e9a1ffbfdcda87b22c91b3
tomorobinson <kwp_tk@hotmail.com>
2018-09-27 22:15:27 +0900

    2nd commit

--- app/src/main/java/jp/techacademy/tomokazu/kawano/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/tomokazu/kawano/autoslideshowapp/MainActivity.java
@@ -13,19 +13,25 @@ import android.os.Build;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.util.Log;
+import android.os.Handler;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
+    private static final String PLAY = "再生";
     private static final String STOP = "停止";
-    String buttonStatus = "再生";
+    int buttonStatus = 0; //0:Play, 1:Stop
 
     ImageView imageView;
     Button nextButton;
     Button playButton;
     Button backButton;
+    Cursor cursor;
+    Timer mTimer;
 
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,71 +42,69 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                // 許可されている
-                imageView = (ImageView) findViewById(R.id.imageView);
-
-                //画像を初期表示
-                setFirstImage();
-
-                //進むボタン押下時の処理
-                nextButton = (Button) findViewById(R.id.next_button);
-                nextButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        setNexImage();
-                    }
-                });
-
-                //再生/停止ボタン押下時の処理
-                playButton = (Button) findViewById(R.id.play_button);
-
-                //戻るボタン押下時の処理
-                backButton = (Button) findViewById(R.id.back_button);
-                backButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        setPreviousImage();
-                    }
-                });
+                // 許可されていれば、ID紐付け
+                initialProcess();
 
             } else {
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
             }
-            // Android 5系以下の場合
+            // Android 5系以下の場合、無条件にID紐付け
         } else {
-            imageView = (ImageView) findViewById(R.id.imageView);
-
-            //画像を初期表示
-            setFirstImage();
-
-            //進むボタン押下時の処理
-            nextButton = (Button) findViewById(R.id.next_button);
-            nextButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    setNexImage();
-                }
-            });
-
-            //再生/停止ボタン押下時の処理
-            playButton = (Button) findViewById(R.id.play_button);
-
-            //戻るボタン押下時の処理
-            backButton = (Button) findViewById(R.id.back_button);
-            backButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    setPreviousImage();
-                }
-            });
+            initialProcess();
         }
     }
 
-    private void setFirstImage() {
-        // 画像の情報を取得する
+    private void initialProcess() {
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(this);
+
+        playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(this);
+
+        backButton = (Button) findViewById(R.id.back_button);
+        backButton.setOnClickListener(this);
+
+        //画像ライブラリの情報取得及び初期画像の設定
+        getContentsInfo();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //ボタンの判定
+        switch (v.getId()) {
+            case R.id.next_button:
+                //次へボタン押下時の処理
+                setNextImage();
+                break;
+
+            case R.id.back_button:
+                //戻るボタン押下時の処理
+                setPreviousImage();
+                break;
+
+            case R.id.play_button:
+                if (buttonStatus == 0) {
+                    //再生時
+                    playSlideShow();
+                } else {
+                    //停止時
+                    stopSlideShow();
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void getContentsInfo() {
+        // ContentResolverを使用して画像ライブラリの情報を取得する
         ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
@@ -114,59 +118,74 @@ public class MainActivity extends AppCompatActivity {
             Long id = cursor.getLong(fieldIndex);
             Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-            Log.d("GETURI", "URI : " + imageUri.toString());
+            // 初期画像を設定
             imageView.setImageURI(imageUri);
         }
-        cursor.close();
     }
 
-    private void setNexImage() {
-        // 画像の情報を取得する
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        );
+    private void setNextImage() {
+        // 次の画像を取得
 
-        if (cursor.isLast()) {
-            setFirstImage();
-        } else {
-            cursor.moveToNext();
-            // indexからIDを取得し、そのIDから画像のURIを取得する
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-            imageView.setImageURI(imageUri);
-        }
-        cursor.close();
-    }
-
-    private void setPreviousImage() {
-        // 画像の情報を取得する
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        );
-
-        if (cursor.isFirst()) {
-            cursor.moveToLast();
-        } else {
-            cursor.moveToNext();
+        if (!cursor.moveToNext()) {
+            cursor.moveToFirst();
         }
 
-        // indexからIDを取得し、そのIDから画像のURIを取得する
         int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
         Long id = cursor.getLong(fieldIndex);
         Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
         imageView.setImageURI(imageUri);
+    }
 
+    private void setPreviousImage() {
+        // 前の画像を取得
+
+        if (!cursor.moveToPrevious()) {
+            cursor.moveToLast();
+        }
+
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        imageView.setImageURI(imageUri);
+    }
+
+    private void slideShow() {
+        // サブスレッドを作成
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    // UIスレッドへ描画依頼
+                    @Override
+                    public void run() {
+                        setNextImage();
+                    }
+                });
+            }
+        }, 2000, 2000);
+    }
+
+    private void playSlideShow() {
+        playButton.setText(STOP);
+        nextButton.setEnabled(false);
+        backButton.setEnabled(false);
+        slideShow();
+        buttonStatus = 1; //ボタンステータスを停止に変更
+    }
+
+    private void stopSlideShow() {
+        mTimer.cancel();
+        mTimer = null;
+        playButton.setText(PLAY);
+        nextButton.setEnabled(true);
+        backButton.setEnabled(true);
+        buttonStatus = 0; //ボタンステータスを再生に変更
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
         cursor.close();
     }
 }
